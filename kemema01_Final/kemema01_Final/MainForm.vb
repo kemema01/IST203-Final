3d60cafccd87335c59c876dfc1eb2d428c7c16a0
kemema01 <kemema01@students.ipfw.edu>
2018-03-30 03:29:44 -0400

    More/most forms created - little/no design work.

--- kemema01_Final/kemema01_Final/MainForm.vb
+++ kemema01_Final/kemema01_Final/MainForm.vb
@@ -1,16 +1,24 @@
-﻿Public Class MainForm
+﻿Option Explicit On
+Option Strict On
+
+Public Class MainForm
+
+    'Begin decision making process.
     Private Sub btnBegin_Click(sender As Object, e As EventArgs) Handles btnBegin.Click
-
+        AttendanceForm.ShowDialog()
     End Sub
 
+    'Display history.
     Private Sub btnHistory_Click(sender As Object, e As EventArgs) Handles btnHistory.Click
-
+        HistoryForm.ShowDialog()
     End Sub
 
+    'Options - for CRUD operations.
     Private Sub btnOptions_Click(sender As Object, e As EventArgs) Handles btnOptions.Click
-
+        OptionsForm.ShowDialog()
     End Sub
 
+    'Exit.
     Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
         Me.Close()
     End Sub
---
+++ kemema01_Final/kemema01_Final/WhatSoundsGoodForm.vb
@@ -0,0 +1,6 @@
+﻿Option Strict On
+Option Explicit On
+
+Public Class WhatSoundsGoodForm
+
+End Class
