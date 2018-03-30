cb15ac8ad6aa0ada32a1fe28275782f2d9c1efc5
kemema01 <kemema01@students.ipfw.edu>
2018-03-30 02:24:32 -0400

    Form1 renamed(MainForm), preliminary design.

---
+++ kemema01_Final/kemema01_Final/MainForm.Designer.vb
@@ -0,0 +1,102 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MainForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnBegin = New System.Windows.Forms.Button()
+        Me.btnOptions = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.btnHistory = New System.Windows.Forms.Button()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.SuspendLayout()
+        '
+        'btnBegin
+        '
+        Me.btnBegin.Location = New System.Drawing.Point(22, 183)
+        Me.btnBegin.Name = "btnBegin"
+        Me.btnBegin.Size = New System.Drawing.Size(75, 30)
+        Me.btnBegin.TabIndex = 0
+        Me.btnBegin.Text = "&Begin"
+        Me.btnBegin.UseVisualStyleBackColor = True
+        '
+        'btnOptions
+        '
+        Me.btnOptions.Location = New System.Drawing.Point(184, 183)
+        Me.btnOptions.Name = "btnOptions"
+        Me.btnOptions.Size = New System.Drawing.Size(75, 30)
+        Me.btnOptions.TabIndex = 1
+        Me.btnOptions.Text = "&Options"
+        Me.btnOptions.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(265, 183)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 30)
+        Me.btnExit.TabIndex = 2
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'btnHistory
+        '
+        Me.btnHistory.Location = New System.Drawing.Point(103, 183)
+        Me.btnHistory.Name = "btnHistory"
+        Me.btnHistory.Size = New System.Drawing.Size(75, 30)
+        Me.btnHistory.TabIndex = 3
+        Me.btnHistory.Text = "&History"
+        Me.btnHistory.UseVisualStyleBackColor = True
+        '
+        'Panel1
+        '
+        Me.Panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Panel1.Location = New System.Drawing.Point(12, 12)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(334, 165)
+        Me.Panel1.TabIndex = 4
+        '
+        'MainForm
+        '
+        Me.AcceptButton = Me.btnBegin
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CancelButton = Me.btnExit
+        Me.ClientSize = New System.Drawing.Size(358, 234)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.btnHistory)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnOptions)
+        Me.Controls.Add(Me.btnBegin)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "MainForm"
+        Me.Text = "The Decider"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btnBegin As Button
+    Friend WithEvents btnOptions As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents btnHistory As Button
+    Friend WithEvents Panel1 As Panel
+End Class
---
+++ kemema01_Final/kemema01_Final/MainForm.vb
@@ -0,0 +1,17 @@
+﻿Public Class MainForm
+    Private Sub btnBegin_Click(sender As Object, e As EventArgs) Handles btnBegin.Click
+
+    End Sub
+
+    Private Sub btnHistory_Click(sender As Object, e As EventArgs) Handles btnHistory.Click
+
+    End Sub
+
+    Private Sub btnOptions_Click(sender As Object, e As EventArgs) Handles btnOptions.Click
+
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+End Class
